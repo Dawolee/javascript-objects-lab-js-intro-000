1accfc768b2af16b3fea15c9bcb265060ae51e3b
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-17 20:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, val) {
   obj[key] = val;
   return obj;
 }
+
+function deleteFromObjectByKey(obj, key) {
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+  return newObj;
+}
